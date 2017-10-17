ecaadbf33075b5312160e43d48b60cc84ee5baf6
Carolyn Jillson <carolynj70@gamil.com>
2017-10-17 13:27:31 -0400

    Just getting set-up

---
+++ MadLibs/Story.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MadLibs
+{
+    class Story
+    {
+        string title;
+        string storyText;
+        List<Word> words;
+
+        public Story(string filePath)
+        {
+            // will read in text from file I/O and call method to prepare the word list
+        }
+    }
+}
---
+++ MadLibs/Word.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MadLibs
+{
+    class Word
+    {
+        int wordType;
+        string text;
+
+
+        public Word(int WordType)
+        {
+            this.wordType = WordType;
+        }
+    }
+}
